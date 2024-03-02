4395ac3f4a42ada5d398ec2e1ae29cd9c30fd5a5
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-02 20:01:54 -0300

    atualização

---
+++ variaveisEOperadores.js
@@ -0,0 +1,10 @@
+//let é uma váriavel e podemos mudar os valores que foram colocados inicialmente
+let variavel = 10;
+
+variavel = 20 * 10;
+// const também é uma váriavel, porém é utilizada para declarar uma váriavel cujo valor é fixo, ou seja, uma vez atribuído um valor a uma constante, esse valor não pode ser mais alterado posteriormente.
+const pi = 3.14;
+
+console.log(variavel) //invocando uma função para mostrar no console
+console.log(pi)
+
