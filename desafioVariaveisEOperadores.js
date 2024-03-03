b9f8b672deac8dbb5a7fb026545f9f05c8cb8e02
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-03 19:38:14 -0300

    atualização

---
+++ desafioVariaveisEOperadores.js
@@ -0,0 +1,10 @@
+let valorCombustivel = 5.85;
+let KmPorLitro = 10;
+let distanciaEmKm = 100;
+
+
+let gastoComViagem = (distanciaEmKm / KmPorLitro) * valorCombustivel;
+
+ console.log(gastoComViagem);
+
+
