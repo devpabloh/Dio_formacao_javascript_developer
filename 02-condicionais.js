644f34f75f070d45e97c995e3fb882347d4449da
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-03 23:14:01 -0300

    atualização

--- 02-condicionais.js
+++ 02-condicionais.js
@@ -3,6 +3,8 @@
 const eNumeroPar = (numero % 2) === 0;
 console.log(eNumeroPar) */
 
+// fazendo usando o operador tenário
+
 let parOuImpar = 15;
 
 let teste = parOuImpar % 2 === 0 ? "true" : "false";
