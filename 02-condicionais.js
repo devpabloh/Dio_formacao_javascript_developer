90732893f44ed782d16f959a951bf4d40e8891e7
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-03 23:13:18 -0300

    atualização

--- 02-condicionais.js
+++ 02-condicionais.js
@@ -1,4 +1,10 @@
-const numero = 23
+/* const numero = 23
 
 const eNumeroPar = (numero % 2) === 0;
-console.log(eNumeroPar)
+console.log(eNumeroPar) */
+
+let parOuImpar = 15;
+
+let teste = parOuImpar % 2 === 0 ? "true" : "false";
+
+console.log(teste)
