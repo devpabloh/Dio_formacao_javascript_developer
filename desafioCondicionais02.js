9d7478cab744826ddd4d96d80595654017597853
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-05 09:07:07 -0300

    atualização

--- desafioCondicionais02.js
+++ desafioCondicionais02.js
@@ -6,17 +6,34 @@
  - média entre 5 e 7, recuperação; e
  - média maior que 7, aprovado.
 */
+//usando variáveis 
+let nota4 = 8;
+let nota5 = 7;
+let nota6 = 7;
 
-function notas (n1, n2, n3){
-    let media = (n1 + n2 + n3) / 3
+let media1 = (nota4 + nota5 + nota6) / 3;
+
+/* console.log(media1); */
+
+if(media1 < 5){
+    console.log(`Sua média é ${media1.toFixed(2)}, você foi reprovado`)
+} else if(media1 <=5 && media1 <= 7){
+    console.log(`Sua média é ${media1.toFixed(2)}, você está em recuperação`)
+} else {
+    console.log(`Sua média é ${media1.toFixed(2)}, você foi aprovado, parabéns!`)
+}
+
+//usando funções
+/* function notas (n1, n2, n3){
+    let media = (n1 + n2 + n3) / 3;
     if(media < 5){
-        return ("você foi reprovado")
+        return ("você foi reprovado");
     
     } else if(media === 6){
-        return ("você está na recuperação")
+        return ("você está na recuperação");
     
     } else {
-        return ("Você foi aprovado")
+        return ("Você foi aprovado");
     }
 }
-console.log(notas(6,6,6))
+console.log(notas(6,6,6)); */
