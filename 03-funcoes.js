306df1794101cfee106693281a589828635186df
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-06 12:01:27 -0300

    atualização

---
+++ 03-funcoes.js
@@ -0,0 +1,7 @@
+
+function incrementarJuros(valor, percentualJuros){
+    const valorDeAcrescimo = (percentualJuros / 100) * valor;
+    return valor + valorDeAcrescimo;
+}
+
+console.log(incrementarJuros(100,10));
