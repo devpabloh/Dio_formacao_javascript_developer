7a62fdb44d7cddfbacca482bb086d295c104af16
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-07 16:52:33 -0300

    Update desafioCondicionais04.js

--- desafioCondicionais04.js
+++ desafioCondicionais04.js
@@ -8,7 +8,34 @@
 4 - Acima de duas vezes, preço normal da etiqueta mais juros de 10%.
  */
 
-let preco = 250;
+function aplicarDesconto(valor, desconto){
+   
+    return (valor - (valor * (desconto/100)));
+    
+}
+
+function aplicarAcrescimo(valor,acrescimo){
+    return (valor + (valor * (acrescimo/100)));
+}
+
+let ValorDasCompras = 100;
+let formasDePagamento = 4;
+
+
+if(formasDePagamento === 1){
+    console.log(`Já que a sua opção de pagamento foi débito você ganhou um desconto de 10%, com esse desconto o valor total à pagar é de R$${aplicarDesconto(ValorDasCompras,10)}.`)
+
+}else if(formasDePagamento === 2){
+    console.log(`Já que a sua opção de pagamento foi dinheiro ou pix você ganhou um desconto de 15%, com esse desconto o valor total a pagar é de R$${aplicarDesconto(ValorDasCompras,15)}.`)
+}else if(formasDePagamento === 4){
+    console.log(`Já que a sua opção de pagamento foi parcelado acima de duas vezes, você terá um valor de 10% adicionado ao preço, com esse adicional o valor total a pagar é de R$${aplicarAcrescimo(ValorDasCompras,10)}.`)
+}else{  
+    console.log(`Pagamento normal da etiqueta a que equivale à R$${ValorDasCompras}.`)
+}
+
+
+
+/* let preco = 250;
 let formasDePagamento = 4
 let debito = preco * 10/100
 let dinheiroPix = preco * 15/100
@@ -24,5 +51,5 @@ if(formasDePagamento === 1){
     console.log(`Já que a sua opção de pagamento foi parcelado acima de duas vezes, você terá um valor de 10% adicionado ao preço, que equivale à R$${divididoAcimaDeDuasVezes}, com esse adicional o valor total a pagar é de R$${preco + divididoAcimaDeDuasVezes}.`)
 }else{
     console.log(`Pagamento normal da etiqueta a que equivale à R$${preco}.`)
-}
+} */
 
