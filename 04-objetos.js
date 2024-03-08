1176effc5598b6ce2f0de4cd34a9d061cef8bf47
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-07 21:36:32 -0300

    atualização

---
+++ 04-objetos.js
@@ -0,0 +1,15 @@
+// Objeto é uma estrutura de dados que permite armazenar e organizar dados de várias formas. Um objeto é uma coleção de pares chave-valor, lembrando que os objetos em javascript são dinâmicos, o que significa que podemos adicionar, remover e modificar propriedades e métodos de um objeto mesmo depois de ele ter sido criado.
+
+const pablo = {
+    nome: 'Pablo Henrique',
+    idade: 30
+
+}
+
+console.log(pablo.nome)
+console.log(pablo.idade)
+
+
+pablo.altura = 1.85;
+
+console.log(pablo)
