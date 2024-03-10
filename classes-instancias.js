a0d787667405a04c56c155d934ea4cf142a5eb2a
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-10 16:06:30 -0300

    atualização

--- classes-instancias.js
+++ classes-instancias.js
@@ -1,4 +1,4 @@
-/* class pessoa {
+ class pessoa {
     nome;
     idade;
 
@@ -7,14 +7,14 @@
     }
 }
 
-const pablo = new pessoa
+/* const pablo = new pessoa
 pablo.nome = "Pablo Henrique";
 pablo.idade = 30;
 
 console.log(pablo.nome)
-console.log(pablo.idade) */
+console.log(pablo.idade)  */
 
-class pessoa {
+/* class pessoa {
     nome;
     idade;
     anoDeNascimento;
@@ -26,4 +26,19 @@ class pessoa {
     descrever(){
     console.log(`Meu nome é ${this.nome}, minha idade é ${this.idade}, eu nasci em ${this.anoDeNascimento}`)}
     
+} */
+
+function compararPessoas(p1,p2){
+    if(p1.idade > p2.idade){
+        console.log(`A idade de ${p1.nome} é maior que a idade de ${p2.nome}, logo ele é mais velho`)
+    } else if(p2.idade > p1.idade){
+        console.log(`A idade de ${p2.nome} é maior que a idade de ${p1.nome}, logo ele é mais velho`)
+    }else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`)
+    }
 }
+
+const pablo = new pessoa('Pablo',30);
+const yuri = new pessoa('Yuri', 25)
+
+compararPessoas(yuri,pablo);
--- testes.js
+++ testes.js
@@ -1,11 +1,11 @@
-const objeto = {
-    nome: 'Pablo Henrique',
-    idade: 30,
-    descrever: function(){
-
-        nome = 'Gabriel'
-        console.log(`Meu nome usando a forma literal é ${nome}, essa forma é usando o objeto.nome ${objeto.nome} meu nome usando o this é ${this.nome}`)
+class Pessoa {
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
     }
-
+    descrever(){
+        console.log(`meu nome é ${this.nome} minha idade é ${this.idade}`)
+    }
 }
-objeto.descrever()
+const pablo = new Pessoa('Pablo Henrique', 30)
+pablo.descrever()
