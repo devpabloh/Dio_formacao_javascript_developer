6fdc8167d7869f28425d6f388acc4c730ddf6130
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-11 00:05:54 -0300

    atualização

--- teste2.js
+++ teste2.js
@@ -3,6 +3,11 @@ class carro{
         this.modelo = modelo;
         this.ano = ano;
     }
+
+    descrever(){
+        console.log(`esse é o modelo do carro ${this.modelo} e este é o ano do carro ${this.ano}`)
+    }
 }
 
 const fiat = new carro('fiat uno', 2012)
+fiat.descrever()
