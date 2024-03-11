8b4091cb0d662f65bd64ccd39c31342c9462157d
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-11 11:30:51 -0300

    atualização

--- exercícioClasses.js
+++ exercícioClasses.js
@@ -15,8 +15,13 @@ class Carros {
         this.gastoMedioKM = gastoMedioKM;
 
     }
-
+    custoDaViagem(distanciaEmKM, precoCombustivel){
+        return (`O valor gasto pelo ${this.marca} para realizar esse trajeto é de ${(distanciaEmKM * this.gastoMedioKM)  * precoCombustivel }`)
+    }
 }
 
 // Quando usamos o new para criar um objeto a partir de uma função construtora ou uma classe, estamos criando uma instância dessa função ou classe, que no caso aqui é Fiat Uno, Vermelho, 15, é uma instância da ckasse Carros.
-const fiat = new Carros('Fiat Uno', 'Vermelho', 15)
+const uno = new Carros('Fiat', 'Vermelho', 1/12)
+const palio = new Carros('Fiat', 'prata', 1/10)
+console.log(uno.custoDaViagem(70,5))
+console.log(palio.custoDaViagem(70,5))
