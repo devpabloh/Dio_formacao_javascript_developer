4e0fd11f2a42a4963e453d73293bdb5e08a46148
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-12 19:03:42 -0300

    atualização

--- 05-arrays.js
+++ 05-arrays.js
@@ -4,5 +4,19 @@ const alunos = ['João', 'Vitor', 'Mariana']
 alunos.push('Renan');
 //segunda forma de adicionar algo a um array
 alunos[4] = 'Pablo'
+// Usamos o poppy quando queremos remover algum valor do Array, ela vai tirar o ultimo valor 
+alunos.pop();
 
-console.log(alunos)
+/* console.log(alunos) */
+
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+
+console.log(soma / notas.length);
