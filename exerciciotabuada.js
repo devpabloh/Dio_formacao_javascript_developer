f7a0b1f28db08f8142f71c079cd5472a8545bda4
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-13 23:22:32 -0300

    Update exerciciotabuada.js

--- exerciciotabuada.js
+++ exerciciotabuada.js
@@ -7,7 +7,7 @@ for(let i = 1; i <= 10; i++){
 } */
 
 
-const numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+/* const numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 for(let i=0; i < numeros.length; i++){
     const par = numeros[i]
@@ -15,3 +15,23 @@ for(let i=0; i < numeros.length; i++){
         console.log(`${par} Esse número é par`)
     }
 }
+ */
+
+/* const numerosPares = [];
+
+    for (let i = 0; i < 10; i = i + 2) {
+        numerosPares.push(i);
+    }
+
+    console.log(numerosPares); */
+
+    const numerosPares = [];
+
+    for (let i = 0; i < 10; i++) {
+        const numeroPar = i % 2 === 0;
+        if (numeroPar) {
+            numerosPares.push(i);
+        }
+    }
+
+    console.log(numerosPares);
