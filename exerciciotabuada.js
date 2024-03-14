2346946595338695ccac0988240897e7f76897f3
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-13 22:44:17 -0300

    atualização

--- exerciciotabuada.js
+++ exerciciotabuada.js
@@ -7,4 +7,11 @@ for(let i = 1; i <= 10; i++){
 } */
 
 
+const numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
+for(let i=0; i < numeros.length; i++){
+    const par = numeros[i]
+    if(par % 2 === 0){
+        console.log(`${par} Esse número é par`)
+    }
+}
