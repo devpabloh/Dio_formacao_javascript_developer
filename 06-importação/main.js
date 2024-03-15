71befb3833f2a3e4b3aff6ce673b14992c4d2bd3
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-15 10:46:39 -0300

    atualização

--- 06-importação/main.js
+++ 06-importação/main.js
@@ -2,13 +2,14 @@ const {gets, print} = require('../06-importação/06-importacoes');
 
 const numerosSorteados = [];
 
+/*  Primeira maneira
 for(i=0; i < 5; i++){
     const numeroSorteado = gets();
     numerosSorteados.push(numeroSorteado);
 
 }
 
-let maiorValor = 0
+ let maiorValor = 0
 
 for(let i= 0; i < numerosSorteados.length; i++){
     const numeroSorteado = numerosSorteados[i];
@@ -17,4 +18,17 @@ for(let i= 0; i < numerosSorteados.length; i++){
     }
 }
 
-print(maiorValor)
+print(maiorValor) */ 
+
+// Maneira mais fácil
+
+let maiorValorEncontrado = 0
+
+for(let i = 0; i < 5; i++){
+    const ValoresSorteados = gets()
+    if(ValoresSorteados > maiorValorEncontrado){
+        maiorValorEncontrado = ValoresSorteados
+    }
+}
+
+print(maiorValorEncontrado)
