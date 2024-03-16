21b0bdc73f53b659d70fc605f45df691f09fc2e7
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-15 23:59:21 -0300

    atualização

--- testes.js
+++ testes.js
@@ -1,4 +1,4 @@
-class Pessoa {
+/* class Pessoa {
     nome;
     idade;
 
@@ -13,4 +13,22 @@ function descreverPessoa(pessoa) {
 }
 
 const renan = new Pessoa('Renan', 30);
-console.log(descreverPessoa(renan));
+console.log(descreverPessoa(renan));  */
+
+var primeira = 10
+let segunda = 20
+const terceira = 30
+
+let arrays = []
+
+const objetos = {}
+
+for(){
+
+}
+
+if(){
+
+}else if(){
+
+}else{}
