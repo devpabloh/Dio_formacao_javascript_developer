9af7a86332052b6fa15ce814a4fc3db61556b224
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-19 23:32:24 -0300

    atualização

--- testes.js
+++ testes.js
@@ -1,37 +1,5 @@
-/* class Pessoa {
-    nome;
-    idade;
+const {gets,print} = require("../Dio_formacao_javascript_developer/06-importação/06-importacoes.js")
 
-    constructor(nome, idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-}
-
-function descreverPessoa(pessoa) {
-    console.log(`Nome: ${pessoa.nome}; Idade: ${pessoa.idade};`);
-}
-
-const renan = new Pessoa('Renan', 30);
-console.log(descreverPessoa(renan));  */
-
-var primeira = 10
-let segunda = 20
-const terceira = 30
-
-let arrays = []
-
-const objetos = {}
-
-for(){
-
-}
-
-if(){
-
-}else if(){
-
-}else{
-
-}
+let salarioBruto = gets()
+const adicional = gets()
 
