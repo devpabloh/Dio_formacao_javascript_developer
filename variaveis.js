57c862382733abf1db136120d2f9bef51da3af26
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-04-04 19:49:17 -0300

    atualização

--- variaveis.js
+++ variaveis.js
@@ -1,12 +1,16 @@
-var var1 = 10;
-var var2 = '20';
-
-console.log(var1 + var2)
-
-var teste 
-
-function teste(){
-    console.log ('teste')
+if(true){
+    var var1= 10
 }
+/* 
+var:
+- pode ser reatribuido
+- ele vaza/ transborda, sendo restringido apenas em escopo global ou de função
+let:
+- pode ser reatribuido
+- não vaza / transborda
+- restrinnge a qualquer tipo de bloco
 
-teste()
+const:
+- Não pode ser reatribuido
+- restrinnge a qualquer tipo de bloco
+*/
