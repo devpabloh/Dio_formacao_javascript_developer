b842e682ea8800154b7186d0cfd9a43a1148d5c7
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-04-05 23:51:09 -0300

    Update tiposDeDados.js

--- tiposDeDados.js
+++ tiposDeDados.js
@@ -14,11 +14,12 @@ symbol
 let x = 10;
 x = 'texto';
 
-console.log(10 == '10')
-console.log(10 === '10')
+/* console.log(10 == '10')
+console.log(10 === '10') */
 
 const nome = '';
 
 if(nome){
     console.log('preenchimento')
 }
+
