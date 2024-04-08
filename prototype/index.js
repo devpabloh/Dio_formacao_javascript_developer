0075d0b98bf1e4f71c3a3de86130d8df22fe3d63
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-04-08 11:49:37 -0300

    Create index.js

---
+++ prototype/index.js
@@ -0,0 +1,31 @@
+/* 
+
+Prototipagem: É um mecanismo fundamental em JavaScript que permite a herança de propriedades e métodos entre objetos.
+
+Protótipo: É um objeto que serve como modelo para a criação de outros objetos. Quando um objeto é criado a partir de outro, ele herda as propriedades e métodos definidos no protótipo.
+
+como funciona o __proto__:
+
+Cada objeto em JavaScript tem um protótipo interno (oculto por padrão) acessível através de __proto__.
+O protótipo é outro objeto que contém propriedades e métodos.
+Quando você tenta acessar uma propriedade em um objeto, o JavaScript primeiro verifica se ela existe diretamente no objeto.
+Se a propriedade não for encontrada, o JavaScript percorre a cadeia de protótipos (o objeto pai e seus próprios protótipos) até encontrar a propriedade ou chegar ao protótipo final (обычный объект (obychnyy obiekt) em russo, que significa "objeto comum" sem protótipo).
+
+ */
+
+
+
+// Lmebrando que objetos são uma coleção dinâmica de chave e valor
+
+const pessoa = {
+    genero: 'Masculino'
+}
+
+const pablo = {
+    nome: 'Pablo',
+    idade: 30,
+    __proto__: pessoa
+}
+
+
+console.log(pablo.genero)
