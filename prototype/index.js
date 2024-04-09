8c908ea1b0975297df166f9b96cdf3a295c9094e
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-04-09 13:56:47 -0300

    Update index.js

--- prototype/index.js
+++ prototype/index.js
@@ -32,6 +32,22 @@ console.log(pablo.genero) */
 
 // funções contrutoras, começamos usando letra maiusculas no nome da função para indicar que estamos criando uma função construtora
 
-function Pessoa (nome, idade){
+function Pessoa1(nome, idade){
+    this.nome = nome
+    this.idade = idade
+}
 
+Pessoa1.prototype.falar = function(){
+    console.log(`Meu nome é: ${this.nome}`) // lembrando de que quando estamos lidando com função construtora o this se refere sempre a função principal.
+}
+// Essa função construtora é a mesma coisa da de cima
+class pessoa {
+    constructor(nome, idade){
+        this.nome = nome
+        this.idade = idade
+
+    }
+    falar(){
+        console.log(`Meu nome é: ${this.nome}`)  // lembrando de que quando estamos lidando com função construtora o this se refere sempre a função principal.
+    }
 }
