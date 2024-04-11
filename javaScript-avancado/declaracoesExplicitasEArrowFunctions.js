cdfe1c36b704fb82b19e0d39987d986df86a7f62
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-04-10 23:37:38 -0300

    Update declaracoesExplicitasEArrowFunctions.js

--- javaScript-avancado/declaracoesExplicitasEArrowFunctions.js
+++ javaScript-avancado/declaracoesExplicitasEArrowFunctions.js
@@ -1,11 +1,20 @@
-//Declaração explicita
+//Declaração explicita  
 
 function funcao1(){
-
+    console.log(this)
 }
 
 //arrow function
 
 const funcao2 = () => {
-
+    console.log(this)
 }
+
+const pablo = {
+    nome: 'Pablo',
+    funcao1,
+    funcao2
+}
+
+pablo.funcao1()
+pablo.funcao2()
