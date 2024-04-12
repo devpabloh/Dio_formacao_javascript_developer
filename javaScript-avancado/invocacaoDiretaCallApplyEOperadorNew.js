3cbd0995e1afe1cfff1ca40b17629bb3f608bb84
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-04-11 22:52:00 -0300

    Update invocacaoDiretaCallApplyEOperadorNew.js

--- javaScript-avancado/invocacaoDiretaCallApplyEOperadorNew.js
+++ javaScript-avancado/invocacaoDiretaCallApplyEOperadorNew.js
@@ -6,14 +6,15 @@ const pessoa = {
 
 function gritar(prefixo){
     console.log(prefixo, this.nome)
+    
 }
 // primeira forma de invocar a função
-gritar()
+gritar('olá')
 
 // Segunda forma de invocar a função, lembrando que as funções são objetos, logo pode ser usados metodos para invocar diretamente essa função, que são os seguintes
 
-gritar.apply(pessoa, ['Olá']) // no uso do apply passamos primeiro o contexto e depois passamos os atributos da função
+gritar.apply(pessoa, ['Olá']) // no uso do apply passamos primeiro o contexto e depois passamos os atributos da função através de um array []
 
 // Terceira forma através do método
 
-gritar.call()
+gritar.call(pessoa,'Oláááá') // no uso do call passamos primeiro o contexto e depois passamos os atributos/argumentos, mas diferente do apply esse não precisa usar um array
