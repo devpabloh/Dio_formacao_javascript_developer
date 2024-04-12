a40d3bf195d861ae4590ee69ef0ddc7f676ed5db
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-04-11 22:29:06 -0300

    Update invocacaoDiretaCallApplyEOperadorNew.js

--- javaScript-avancado/invocacaoDiretaCallApplyEOperadorNew.js
+++ javaScript-avancado/invocacaoDiretaCallApplyEOperadorNew.js
@@ -1,5 +1,13 @@
 function teste(){
     console.log('teste')
 }
-
+// primeira forma de invocar a função
 teste()
+
+// Segunda forma de invocar a função, lembrando que as funções são objetos, logo pode ser usados metodos para invocar diretamente essa função, que são os seguintes
+
+teste.apply
+
+// Terceira forma através do método
+
+teste.call
