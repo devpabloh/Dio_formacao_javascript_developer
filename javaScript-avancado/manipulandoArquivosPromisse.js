b494381bed6fd1162f4dc63ef47017094691a566
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-04-15 22:48:45 -0300

    a

--- javaScript-avancado/manipulandoArquivosPromisse.js
+++ javaScript-avancado/manipulandoArquivosPromisse.js
@@ -1,13 +1,15 @@
 // No node temos um modulo para fazer leitura de arquivos, que seria o require
 
 const fs = require('fs')
+const path = require('path')
 
-const promesaDeLeituraDoArquivo = fs.promises.readFile('tarefas.csv') // ler arquivo através das promises
+const filepath = path.resolve(__dirname, 'tarefas.csv')
 
-promesaDeLeituraDoArquivo.then((arquivo)=>{
-    console.log(arquivo.toString('utf8'))
-}).catch((error)=>{
-    console.log('Deu ruim!', error)
-})
+async function buscarArquivo(){
+    const arquivo = await fs.promises.readFile(filepath)
+    const textoDoArquivo = arquivo.toString('utf8')
+    console.log(textoDoArquivo)
+}
 
+buscarArquivo()
 
